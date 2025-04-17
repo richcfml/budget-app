68df47b282f746f8fd065c5a36986c5949f2b460
Richard Ardila <richcfml@gmail.com>
2025-04-17 08:41:38 -0500

    Add Transaction information and updated relationship to other models

---
+++ app/Models/Transaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    /** @use HasFactory<\Database\Factories\TransactionFactory> */
+    use HasFactory;
+
+    protected $fillable = ['amount', 'description', 'date', 'category_id', 'user_id'];
+
+    protected $casts = ['amount' => 'decimal:2', 'date' => 'date'];
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
